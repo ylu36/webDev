5e072dde51e5fef592ecaa58c7e4a536bba979ca
Yuanchen Lu <ylu36@ncsu.edu>
2017-12-30 12:20:15 -0500

    texture change

--- guessGame.js
+++ guessGame.js
@@ -1,21 +1,49 @@
 var numSquares = 6;
-var colors = generateRandomColors(numSquares);
+var colors = [];
+var pickedColor;
 var colorDisplay = document.getElementById("colorDisplay");
 var squares = document.querySelectorAll(".square");
 var messageDisplay = document.querySelector("#message");
-var pickedColor = pickColor();
 var resetButton = document.querySelector("#reset");
 var modeButtons = document.querySelectorAll(".mode");
 var h1 = document.querySelector("h1");
 
-for(var i = 0; i < modeButtons.length; i ++) {
-  modeButtons[i].addEventListener("click", function() {
-    modeButtons[0].classList.remove("selected");
-    modeButtons[1].classList.remove("selected");
-    this.classList.add("selected");
-    (this.textContent == "Easy")? numSquares = 3: numSquares = 6;
-    reset();
-  });
+init();
+
+function init() {
+  setUpModeBtn();
+  setUpSquares();
+  reset();
+}
+
+function setUpModeBtn(){
+  for(var i = 0; i < modeButtons.length; i ++) {
+    modeButtons[i].addEventListener("click", function() {
+      modeButtons[0].classList.remove("selected");
+      modeButtons[1].classList.remove("selected");
+      this.classList.add("selected");
+      (this.textContent == "Easy")? numSquares = 3: numSquares = 6;
+      reset();
+    });
+  }
+}
+
+function setUpSquares(){
+  for(var i = 0; i < squares.length; i ++) {
+    squares[i].addEventListener("click", function(){
+      var clickedColor = this.style.backgroundColor;
+      if(clickedColor == pickedColor) {
+          changeColor(clickedColor);
+			    h1.style.background = clickedColor;
+          messageDisplay.textContent = "Correct";
+          resetButton.textContent = "Play Again?";
+      }
+      else {
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again";
+      }
+    });
+  }
 }
 
 function reset() {
@@ -36,26 +64,9 @@ function reset() {
   h1.style.background = "steelblue";
 }
 
-colorDisplay.textContent = pickedColor;
 resetButton.addEventListener("click", function(){
   reset();
 });
-
-for(var i = 0; i < squares.length; i ++) {
-  squares[i].style.backgroundColor = colors[i];
-  squares[i].addEventListener("click", function(){
-    var clickedColor = this.style.backgroundColor;
-    if(clickedColor == pickedColor) {
-        changeColor(clickedColor);
-        messageDisplay.textContent = "Correct";
-        resetButton.textContent = "Play Again?";
-    }
-    else {
-      this.style.backgroundColor = "steelblue";
-      messageDisplay.textContent = "Try Again";
-    }
-  });
-}
 
 function generateRandomColors(n) {
   var arr = [];
