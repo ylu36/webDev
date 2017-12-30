ff62157500bd12df9461ab7265cd3cdda77142b1
Yuanchen Lu <ylu36@ncsu.edu>
2017-12-29 22:28:04 -0500

    use random colors

--- guessGame.js
+++ guessGame.js
@@ -1,14 +1,7 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(255, 0, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 255, 0)",
-]
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var messageDisplay = document.querySelector("#message");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
 for(var i = 0; i < squares.length; i ++) {
@@ -16,6 +9,7 @@ for(var i = 0; i < squares.length; i ++) {
   squares[i].addEventListener("click", function(){
     var clickedColor = this.style.backgroundColor;
     if(clickedColor == pickedColor) {
+        changeColor(clickedColor);
         messageDisplay.textContent = "Correct";
     }
     else {
@@ -23,4 +17,31 @@ for(var i = 0; i < squares.length; i ++) {
       messageDisplay.textContent = "Try Again";
     }
   });
+}
+
+function generateRandomColors(n) {
+  var arr = [];
+  for(var i = 0; i < n; i ++) {
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor() {
+  var r = Math.floor(Math.random()*256);
+  var g = Math.floor(Math.random()*256);
+  var b = Math.floor(Math.random()*256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function changeColor(color) {
+  for(var i = 0; i < colors.length; i ++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  var c = Math.floor(Math.random()*colors.length);
+  console.log(c);
+  return colors[c];
 }
