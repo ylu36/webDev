0de44c9c3cd13fa09c9ed25c491595d8f43ab2c3
Yuanchen Lu <ylu36@ncsu.edu>
2017-12-31 14:07:33 -0500

    can add todos

--- todo.js
+++ todo.js
@@ -1,10 +1,19 @@
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
   $(this).parent().fadeOut(500,function(){
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if(event.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append("<li><span>X</span> "+todoText+"</li>");
+  }
 });
