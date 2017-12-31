42b9d0a5789b8a4ecc46ff4b941894c64b2f0e11
Yuanchen Lu <ylu36@ncsu.edu>
2017-12-31 14:53:59 -0500

    styling changes

--- todo.js
+++ todo.js
@@ -14,6 +14,10 @@ $("input[type='text']").keypress(function(event) {
     var todoText = $(this).val();
     $(this).val("");
     //create a new li and add to ul
-    $("ul").append("<li><span>X</span> "+todoText+"</li>");
+    $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> "+todoText+"</li>");
   }
+});
+
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle();
 });
