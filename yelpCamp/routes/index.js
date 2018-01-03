9ef71f722f3854c722bf5246271b3f3a04ebd5e3
Yuanchen Lu <ylu36@ncsu.edu>
2018-01-03 16:11:34 -0500

    layout and basic styling with Bootstrap

--- yelpCamp/routes/index.js
+++ yelpCamp/routes/index.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var campgrounds = [
+    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("server started...");
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
+router.get('/campgrounds', function(req, res, next) {
+    res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+router.post('/campgrounds', function (req, res, next){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+router.get('/campgrounds/new', function(req, res, next) {
+        res.render('new');
+});
 module.exports = router;
