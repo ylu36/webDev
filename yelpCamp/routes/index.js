b082e9e36482b17768097a758b7e2d7f918ed542
Yuanchen Lu <ylu36@ncsu.edu>
2018-01-03 22:34:52 -0500

    connected to DB

--- yelpCamp/routes/index.js
+++ yelpCamp/routes/index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://localhost/yelp_camp",function(){
+   // mongoose.connection.db.dropDatabase();
+});
 var campgroundSchema = new mongoose.Schema({
     name: String,
     image: String
 });
 var Campground = mongoose.model("Campground", campgroundSchema);
+/*
 Campground.create({
-    name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
+    name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
 }, function(err, campground){
     if(err)
         console.log("error");
@@ -16,6 +19,7 @@ Campground.create({
         console.log(campground);
     }
 });
+*/
 var campgrounds = [
     {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
     {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
@@ -35,15 +39,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/campgrounds', function(req, res, next) {
-    res.render('campgrounds', {campgrounds: campgrounds});
+    // res.render('campgrounds', {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampground){
+        if(err) console.log(err);
+        else
+            res.render('campgrounds', {campgrounds: allCampground});
+    });
 });
 
 router.post('/campgrounds', function (req, res, next){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    //campgrounds.push(newCampground);
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err) console.log(err);
+        else {
+            res.redirect('/campgrounds');
+        }
+    });
 });
 
 router.get('/campgrounds/new', function(req, res, next) {
