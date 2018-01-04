0b927c337c465c66be9660e5a2b3284976610388
Yuanchen Lu <ylu36@ncsu.edu>
2018-01-03 22:02:19 -0500

    working on DB

--- yelpCamp/routes/index.js
+++ yelpCamp/routes/index.js
@@ -1,5 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/yelp_camp");
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+var Campground = mongoose.model("Campground", campgroundSchema);
+Campground.create({
+    name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
+}, function(err, campground){
+    if(err)
+        console.log("error");
+    else {
+        console.log(campground);
+    }
+});
 var campgrounds = [
     {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
     {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
