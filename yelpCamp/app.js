4c12a8c5db759f1bf038542e6b5ec7c58376c776
Yuanchen Lu <ylu36@ncsu.edu>
2018-01-05 22:04:25 -0500

    syting change to image padding

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname+"/public"));
 app.use('/', index);
 app.use('/users', users);
 
